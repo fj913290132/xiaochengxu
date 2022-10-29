64685b44569dc324a89adb2ab1484ef4727110ef
fengjie <91329132@qq.com>
2022-10-29 18:50:40 +0800

    完成了tabBar功能的开发

---
+++ pages/my/my.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    你的啊
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
